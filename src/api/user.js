1e6248772afabbd8a215705afbb945832adc328c
chenghongqiang <962691576@qq.com>
2019-05-29 15:03:50 +0800

    Initial commit

---
+++ src/api/user.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/system/user/getByToken',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
+
+export function getListByPage(data) {
+  return request({
+    url: '/system/user/getListByPage',
+    method: 'get',
+    params: { data }
+  })
+}
+
